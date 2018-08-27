291d2e25cbf9e4c25016bf904ffb8b36fda3f132
Oleg <oleg.pav.m@gmail.com>
2018-08-27 12:19:46 +0300

    Init + skeleton + start doc.

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+const core = require('gls-core-service');
+const BasicMain = core.services.BasicMain;
+const stats = core.statsClient;
+
+class Main extends BasicMain {
+    constructor() {
+        super(stats);
+
+        // TODO -
+    }
+}
